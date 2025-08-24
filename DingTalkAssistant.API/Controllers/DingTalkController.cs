439b4c018c7bf703bf874eefa36f3297463034c2
ZhanLai <zhanlai@MacBook.local>
2025-08-24 16:51:56 +0800

    add debug info

--- DingTalkAssistant.API/Controllers/DingTalkController.cs
+++ DingTalkAssistant.API/Controllers/DingTalkController.cs
@@ -1,3 +1,4 @@
+using System.Text.Json.Nodes;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DingTalkAssistant.API.Controllers;
@@ -7,8 +8,10 @@ namespace DingTalkAssistant.API.Controllers;
 public class DingTalkController : Controller
 {
     // GET
-    public IActionResult Assistant()
+    [HttpPost]
+    public IActionResult Assistant([FromBody]JsonObject request)
     {
+        Console.WriteLine(request);
         return Json(new { Code = 200 });
     }
 }
